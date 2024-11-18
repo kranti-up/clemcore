a65e6878a37997535a768ba6a68adc1b9f3e5cb4
Anne Beyer <anne.beyer@uni-potsdam.de>
2024-11-18 23:12:47 +0100

    [scripts] update run_benchmark.sh

--- scripts/run_benchmark.sh
+++ scripts/run_benchmark.sh
@@ -2,19 +2,21 @@
 # Usage: scripts/run_benchmark.sh
 
 source prepare_path.sh
-mkdir "logs"
+mkdir -p logs
 
-version="v2.0"
+version="v1.6_control"
 games=(
 "taboo"
-# "referencegame"
-# "imagegame"
-# "wordle"
-# "wordle_withclue"
-# "wordle_withcritic"
-# "privateshared"
+"referencegame"
+"imagegame"
+#"wordle"
+#"wordle_withclue"
+#"wordle_withcritic"
+"privateshared"
+"codenames"
 )
 models=(
+"llama-70b"
 "mock"
 #"claude-3-opus-20240229"
 #"gpt-4-turbo-2024-04-09"
@@ -36,15 +38,12 @@ for game in "${games[@]}"; do
     # currently, instances.json is the default file for the current benchmark version
     # older files are being renamed retrospectively, but the framework also allows
     # to input a specific instances file, so we could also use the version number here like this: -i instances_"${version}".json
-    { time python3 scripts/cli.py run -g "${game}" -m "${model}" -r "../results/${version}"; } 2>&1 | tee logs/runtime."${game}"."${model}".log
-    { time python3 scripts/cli.py transcribe -g "${game}" -r "../results/${version}"; } 2>&1 | tee logs/runtime.transcribe."${game}".log
-    { time python3 scripts/cli.py score -g "${game}" -r "../results/${version}"; } 2>&1 | tee logs/runtime.score."${game}".log
+    { time python3 clemcore/cli.py run -g "${game}" -m "${model}" -r "results/${version}"; } 2>&1 | tee logs/runtime."${game}"."${model}".log
+    { time python3 clemcore/cli.py transcribe -g "${game}" -r "results/${version}"; } 2>&1 | tee logs/runtime.transcribe."${game}".log
+    { time python3 clemcore/cli.py score -g "${game}" -r "results/${version}"; } 2>&1 | tee logs/runtime.score."${game}".log
   done
 done
-
-# Note: -r in cli.py is apparently parsed differently from -p below, so here the path is relative to the current directory
-# (which is assumed to be clembench/, whereas the former takes it to be relative to clembench/framework)
-# This should be adapted (somewhere in the intersection of framework/clemgame/__init__.py and framework/utils/file_utils.py)
+echo "Evaluating results"
 { time python3 evaluation/bencheval.py -p "results/${version}"; }
 
 echo "==================================================="
